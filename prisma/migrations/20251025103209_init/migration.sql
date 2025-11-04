99a020a8804ba0090d20788acec0bc6fd2a13d66
Artem653 <gromboom1325@gmail.com>
2025-11-04 17:12:45 +0200

    ---___---..-..-.---.-.---.--..-.--..-

---
+++ prisma/migrations/20251025103209_init/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `description` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `image` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Post" ("createdAt", "id", "title") SELECT "createdAt", "id", "title" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
